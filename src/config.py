31a261d0de50df3a829d0b2f923817cec2c72310
w0rng <antonabramov2000@gmail.com>
2022-09-27 09:52:37 +0700

    + db

--- src/config.py
+++ src/config.py
@@ -2,4 +2,11 @@ from pydantic import BaseSettings, Field
 
 
 class Config(BaseSettings):
-    db_url: str = Field(..., env="DB_URL")
+    db: str = Field(..., env="POSTGRES_DB")
+    db_user: str = Field(..., env="POSTGRES_USER")
+    db_password: str = Field(..., env="POSTGRES_PASSWORD")
+    db_host: str = Field(..., env="POSTGRES_HOST")
+
+    @property
+    def db_url(self) -> str:
+        return f"postgresql+asyncpg://{self.db_user}:{self.db_password}@{self.db_host}/{self.db}"
