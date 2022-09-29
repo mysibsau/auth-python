d181d86e209a9073985af428c4d87f29f3b07dd8
w0rng <antonabramov2000@gmail.com>
2022-09-29 19:17:20 +0700

    fix db url

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseSettings, Field
 
 
@@ -6,10 +8,13 @@ class Config(BaseSettings):
     db_user: str = Field(..., env="POSTGRES_USER")
     db_password: str = Field(..., env="POSTGRES_PASSWORD")
     db_host: str = Field(..., env="POSTGRES_HOST")
+    _db_url: Optional[str] = Field(None, env="DATABASE_URL")
 
     odoo_db: str = Field(..., env="ODOO_DB")
     odoo_url: str = Field(..., env="ODOO_URL")
 
     @property
     def db_url(self) -> str:
+        if self._db_url is not None:
+            return self._db_url
         return f"postgresql+asyncpg://{self.db_user}:{self.db_password}@{self.db_host}/{self.db}"
