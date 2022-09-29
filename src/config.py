8b9559a1ccfbd2c44e6b4aed964d6a44bee56eb0
w0rng <antonabramov2000@gmail.com>
2022-09-29 19:29:02 +0700

    fix db url

--- src/config.py
+++ src/config.py
@@ -8,13 +8,13 @@ class Config(BaseSettings):
     db_user: str = Field(..., env="POSTGRES_USER")
     db_password: str = Field(..., env="POSTGRES_PASSWORD")
     db_host: str = Field(..., env="POSTGRES_HOST")
-    _db_url: Optional[str] = Field(None, env="DATABASE_URL")
+    _db_url: str = Field("", env="DATABASE_URL")
 
     odoo_db: str = Field(..., env="ODOO_DB")
     odoo_url: str = Field(..., env="ODOO_URL")
 
     @property
     def db_url(self) -> str:
-        if self._db_url is not None:
+        if self._db_url:
             return self._db_url
         return f"postgresql+asyncpg://{self.db_user}:{self.db_password}@{self.db_host}/{self.db}"
